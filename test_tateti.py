33f86c30ec8df0ede252b1fe0fb02b36cd8239a2
karen jesica <karenjesica@gmail.com>
2019-12-20 15:04:51 -0300

    Test para posiciones del tablero validas

--- test_tateti.py
+++ test_tateti.py
@@ -3,3 +3,21 @@ import tateti
 
 def test_quien_comienza():
     assert type(tateti.quien_comienza()) is bool 
+
+def test_posicion_invalida():
+    assert type(tateti.posicion_invalida('a')) is not None
+
+def test_posicion_invalida_type():
+    assert type(tateti.posicion_invalida("5")) is bool
+
+def test_posicion_invalida_numeros_correctos(): 
+    assert tateti.posicion_invalida("5") is True
+
+def test_posicion_invalida_numeros_incorrectos(): 
+    assert tateti.posicion_invalida("11") is False
+
+def test_posicion_invalida_letras(): 
+    assert tateti.posicion_invalida("a") is False
+
+def test_posicion_invalida_caracteres_especiales(): 
+    assert tateti.posicion_invalida("%?") is False
