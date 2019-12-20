0b992974f0805e5f233270e3e240ad08993fc230
karen jesica <karenjesica@gmail.com>
2019-12-20 10:18:25 -0300

    Muestra el tablero vacio y agrego un test para verificar el tipo de dato que devuelve.

--- tateti.py
+++ tateti.py
@@ -4,22 +4,43 @@ def comenzar_partida():
     try:
         print("Comenzar a jugar \n")
         primera_vez = True
-        quien_comienza(primera_vez)
+        pers = quien_comienza()
+        mostrar_tablero(primera_vez, pers, tablero = " "*10)
     except Exception as e:
         print(e)
 
-def quien_comienza(primera_vez):
+def quien_comienza():
     try:
-        tablero = " "*10
+        
         if random.randint(0, 1) == 0:
             pers = False
         else:
             pers = True
-
+        
+        return pers
     except Exception as e:
         print(e)
-  
-  if __name__ == "__main__":
+
+def mostrar_tablero(primera_vez, pers, tablero):
+    try:
+        linea_vertical = "                      |                            |"
+        linea_punteada = '---------------------------------------------------------------------'
+
+        print(linea_vertical)
+        print(linea_vertical)
+        print('7-        ' + tablero[6] + '           | 8-           ' + tablero[7] + '             | 9-           ' + tablero[8])
+        print(linea_punteada)
+        print(linea_vertical)
+        print('4-        ' + tablero[3] + '           | 5-           ' + tablero[4] + '             | 6-           ' + tablero[5])
+        print(linea_vertical)
+        print(linea_punteada)
+        print('1-        ' + tablero[0] + '           | 2-           ' + tablero[1] + '             | 3-           ' + tablero[2])
+        print(linea_vertical)
+        print(linea_vertical)
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
     comenzar_partida()
 
 
---
+++ test_tateti.py
@@ -0,0 +1,5 @@
+import pytest
+import tateti
+
+def test_quien_comienza():
+    assert type(tateti.quien_comienza()) is bool 
